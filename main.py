fbd504fd61d99919bec0dcccd3cfeb6fecd72f3c
alshbg <109930669+alshbg2001@users.noreply.github.com>
2022-08-14 14:51:47 +0300

    Update main.py

--- main.py
+++ main.py
@@ -9,10 +9,10 @@ bot = Client(
     "bot",
     api_id=2033318,
     api_hash='223cde1537f217dda4e16183f47af958',
-    bot_token='5131640606:AAFXnnI19LkTXWEZ0YEVzYM2TA18OFHETDQ'
+    bot_token='5597176415:AAGqPEVWlox5a-hYXPYBsh57DVdl7X55g-M'
 )
 
-Dev = 5098935434
+Dev = 961743188
 
 @bot.on_message(filters.command('start'))
 def start(bot, msg):
